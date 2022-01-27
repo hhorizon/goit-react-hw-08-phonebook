4eca034eafe994b91a1adbb5344e28611652f611
Anatolii Litynskyi <litinskiy.tolik@gmail.com>
2022-01-27 20:48:25 +0200

    v.0.0.1

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+
+import { useEffect } from 'react';
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+
+// import PrivatRoute from 'views/PrivatRoute';
+// import PublicRoute from 'views/PublicRoute';
+// import HomePage from 'views/HomePage';
+// import ContactsPage from 'views/ContactsPage/ContactsPage';
+// import LoginPage from 'views/LoginPage/LoginPage';
+// import RegistrationPage from 'views/RegisterPage/RegistrerPage';
+
+// import { authOperations, authSelectors } from 'redux/auth';
+
+
+export function App() {
+  // const dispatch = useDispatch();
+
+  // useEffect(()=> {
+  //   dispatch(authOperations.refreshCurrentUser())
+  // }, [dispatch])
+  
+  return (<>
+    {/* <button type='click' onClick={() => dispatch(authOperations.logOut())}>Log out</button> */}
+
+    <Routes>
+      <Route path='/' element={<p>Home</p>}></Route>
+      <Route path='/login' element={<p>Login</p>}></Route>
+      <Route path='/register' element={<p>Register</p>}></Route>
+      <Route path='/contacts' element={<p>Contacts</p>}></Route>
+    </Routes>
+  </>);
+};
